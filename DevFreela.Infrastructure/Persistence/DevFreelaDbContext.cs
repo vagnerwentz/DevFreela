c1518dfde3e61e93c944cbff5e753372f65e6b71
Vagner Humberto Wentz <vagnerwentz@Vagners-MacBook-Pro.local>
2022-08-23 23:20:50 -0300

    feat: add entity framework core

--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -1,39 +1,22 @@
 ﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.Infrastructure.Persistence
 {
-    public class DevFreelaDbContext
+    public class DevFreelaDbContext : DbContext
     {
-        public DevFreelaDbContext()
+        public DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options) : base(options)
         {
-            Projects = new List<Project>
-            {
-                new Project("Meu projeto ASP NET Core", "Projeto DevFreela", 1, 1, 10000),
-                new Project("Meu projeto ASP NET Core 2", "Projeto DevFreela 2", 1, 1, 5240),
-                new Project("Meu projeto ASP NET Core 3", "Projeto DevFreela 3", 1, 1, 37900)
-            };
-
-            User = new List<User>
-            {
-                new User("Vagner Wentz", "wentz.vagner@gmail.com", new DateTime(1998, 1, 1)),
-                new User("Bruna Eloisa Wentz", "wentz.eloisa@gmail.com", new DateTime(1998, 1, 1)),
-            };
-
-            Skills = new List<Skill>
-            {
-                new Skill(".NET Core"),
-                new Skill("Apache Kafka"),
-                new Skill("MongoDB"),
-            };
         }
 
-        public List<Project> Projects { get; set; }
+        public DbSet<Project> Projects { get; set; }
 
-        public List<User> User { get; set; }
+        public DbSet<User> User { get; set; }
 
-        public List<Skill> Skills { get; set; }
+        public DbSet<Skill> Skills { get; set; }
 
-        public List<ProjectComment> ProjectComments { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
+
+        public DbSet<ProjectComment> ProjectComments { get; set; }
     }
 }
-
